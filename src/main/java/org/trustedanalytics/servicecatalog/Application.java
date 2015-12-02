e77734f1f774bc98baf128fe56962100e4d62e56
Piotr <piotr.a.chmielinski@intel.com>
2015-12-02 15:10:58 +0100

    Swagger removed.

--- src/main/java/org/trustedanalytics/servicecatalog/Application.java
+++ src/main/java/org/trustedanalytics/servicecatalog/Application.java
@@ -15,14 +15,12 @@
  */
 package org.trustedanalytics.servicecatalog;
 
-import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 
 @SpringBootApplication
 @EnableOAuth2Resource
-@EnableSwagger
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
